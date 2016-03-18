398d88e738bb9983e49feec2bff070be0e731f11
jcppython <jcppython@outlook.com>
2016-03-18 14:41:06 +0800

    添加tmuxifier,并重新修改了安装方法

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -sf tmux.conf $HOME/.tmux.conf
+git clone https://github.com/jimeh/tmuxifier.git plugins/tmuxifier
+ln -sf $PWD/plugins/tmuxifier $HOME/.tmuxifier
+echo "安装tmuxifier"
+echo "请手动进行以下操作"
+echo "将 $HOME/.tmuxifier/bin 添加到 PATH"
+echo '将 eval "$(tmuxifier init -)" 添加到$HOME/.profile, $HOME/.bash_profile, $HOME/.zshrc (添加一个有效可用即可)'
